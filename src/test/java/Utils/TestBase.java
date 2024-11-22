81a11712c93c19cde538165be6e68483c8469fb0
yashika <yashikagoyal377@gmail.com>
2024-11-22 22:53:52 +0530

    second commit

--- src/test/java/Utils/TestBase.java
+++ src/test/java/Utils/TestBase.java
@@ -1,17 +1,29 @@
 package Utils;
 
 import java.io.FileInputStream;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.util.HashMap;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class TestBase {
 
-	public ChromeDriver driver;
-	public ChromeDriver WebDriverManager() throws IOException {
+	public WebDriver driver;
+	public  WebDriver WebDriverManager() throws IOException {
 		
 		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"/src/test/resources/global.properties");
 		Properties prop = new Properties();
@@ -21,11 +33,10 @@ public class TestBase {
 		if (driver == null) {
 			
 			if(prop.getProperty("browser").equalsIgnoreCase("chrome")) { //Here we are checking that the if the browser is chromebrowser then execute below:
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\Yashika goyal\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
 		
-	    ChromeOptions chromeOptions = new ChromeOptions();
-	    chromeOptions.addArguments("--remote-allow-origins=*");
-	   driver = new ChromeDriver(chromeOptions);
+	    
+	   driver = new ChromeDriver();
 			}
 			
 			if(prop.getProperty("browser").equalsIgnoreCase("firefox")) {
